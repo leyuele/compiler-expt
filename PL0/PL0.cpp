8804b69c995b3a7f0b51f6ed40f581f40c8d03b8
leyuele <leyuele@outlook.com>
2026-06-18 10:57:18 +0800

    new_feat1

--- PL0/PL0.cpp
+++ PL0/PL0.cpp
@@ -89,6 +89,8 @@ void init() {
   ssym['/'] = slash;
   ssym['('] = lparen;
   ssym[')'] = rparen;
+  ssym['['] = lbracket;
+  ssym[']'] = rbracket;
   ssym['='] = eql;
   ssym[','] = comma;
   ssym['.'] = period;
@@ -100,28 +102,32 @@ void init() {
   strcpy(&(word[2][0]), "const");
   strcpy(&(word[3][0]), "do");
   strcpy(&(word[4][0]), "end");
-  strcpy(&(word[5][0]), "if");
-  strcpy(&(word[6][0]), "odd");
-  strcpy(&(word[7][0]), "procedure");
-  strcpy(&(word[8][0]), "read");
-  strcpy(&(word[9][0]), "then");
-  strcpy(&(word[10][0]), "var");
-  strcpy(&(word[11][0]), "while");
-  strcpy(&(word[12][0]), "write");
+  strcpy(&(word[5][0]), "for");
+  strcpy(&(word[6][0]), "if");
+  strcpy(&(word[7][0]), "odd");
+  strcpy(&(word[8][0]), "procedure");
+  strcpy(&(word[9][0]), "read");
+  strcpy(&(word[10][0]), "then");
+  strcpy(&(word[11][0]), "to");
+  strcpy(&(word[12][0]), "var");
+  strcpy(&(word[13][0]), "while");
+  strcpy(&(word[14][0]), "write");
   /*设置保留字符号*/
   wsym[0] = beginsym;
   wsym[1] = callsym;
   wsym[2] = constsym;
   wsym[3] = dosym;
   wsym[4] = endsym;
-  wsym[5] = ifsym;
-  wsym[6] = oddsym;
-  wsym[7] = procsym;
-  wsym[8] = readsym;
-  wsym[9] = thensym;
-  wsym[10] = varsym;
-  wsym[11] = whilesym;
-  wsym[12] = writesym;
+  wsym[5] = forsym;
+  wsym[6] = ifsym;
+  wsym[7] = oddsym;
+  wsym[8] = procsym;
+  wsym[9] = readsym;
+  wsym[10] = thensym;
+  wsym[11] = tosym;
+  wsym[12] = varsym;
+  wsym[13] = whilesym;
+  wsym[14] = writesym;
 
   /*设置指令名称*/
   strcpy(&(mnemonic[lit][0]), "lit");
@@ -261,6 +267,9 @@ int getsym() {
     } while (i <= j);
     if (i - 1 > j) {
       sym = wsym[k];
+      if (sym == forsym || sym == tosym) {
+        printf("*** Lexer detected keyword: %s ***\n", id);
+      }
     } else {
       sym = ident;
     }
@@ -288,35 +297,39 @@ int getsym() {
         } else {
           sym = nul; /*不能识别的符号*/
         }
+      } else if (ch == '<') {
+        getchdo;
+        if (ch == '=') {
+          sym = leq;
+          getchdo;
+        } else if (ch == '>') {
+          sym = neqalt;
+          printf("*** Lexer detected keyword: <> ***\n");
+          getchdo;
+        } else {
+          sym = lss;
+        }
       } else {
-        if (ch == '<') /*检测小于或小于等于符号*/
+        if (ch == '>') /*检测大于或大于等于符号*/
         {
           getchdo;
           if (ch == '=') {
-            sym = leq;
+            sym = geq;
             getchdo;
           } else {
-            sym = lss;
+            sym = gtr;
           }
         } else {
-          if (ch == '>') /*检测大于或大于等于符号*/
-          {
-            getchdo;
-            if (ch == '=') {
-              sym = geq;
-              getchdo;
-            } else {
-              sym = gtr;
-            }
-          } else {
-            sym = ssym[ch]; /* 当符号不满足上述条件时，全部按照单字符号处理*/
-            // getchdo;
-            // richard
-            if (sym != period) {
-              getchdo;
-            }
-            // end richard
+          sym = ssym[ch]; /* 当符号不满足上述条件时，全部按照单字符号处理*/
+          if (sym == lbracket || sym == rbracket) {
+            printf("*** Lexer detected symbol: %c ***\n", ch);
           }
+          // getchdo;
+          // richard
+          if (sym != period) {
+            getchdo;
+          }
+          // end richard
         }
       }
     }
--- PL0/PL0.h
+++ PL0/PL0.h
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-#define norw 13   /*关键字个数*/
+#define norw 15   /*关键字个数*/
 #define txmax 100 /*名字表容量*/
 #define nmax 14   /*number的最大位数*/
 #define al 10     /*符号的最大长度*/
@@ -22,12 +22,15 @@ enum symbol {
   oddsym,
   eql,
   neq,
+  neqalt,
   lss,
   leq,
   gtr,
   geq,
   lparen,
   rparen,
+  lbracket,
+  rbracket,
   comma,
   semicolon,
   period,
@@ -44,8 +47,10 @@ enum symbol {
   constsym,
   varsym,
   procsym,
+  forsym,
+  tosym,
 };
-#define symnum 32
+#define symnum 38
 /*-------------*/
 enum object {
   constant,
